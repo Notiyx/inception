6e2eeb20e5c5decae7ae81da1c93776a745d401c
Nokiyx <nokiyxdev@gmail.com>
2025-07-06 13:33:37 -0500

    i;agine ca marche po

--- srcs/requirements/mariadb/tools/installmariadb.sh
+++ srcs/requirements/mariadb/tools/installmariadb.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+set -e
 
-service mysql start
+mysqld_safe --skip-networking &
+sleep 5
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+    CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
+EOSQL
 
 mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
 
--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-sleep 10
+# Attente que MariaDB soit disponible
+until mysql -h mariadb -u "$SQL_USER" -p"$SQL_PASSWORD" -e 'show databases;' > /dev/null 2>&1; do
+  echo "En attente de MariaDB..."
+  sleep 2
+done
+
+# Une fois MariaDB disponible, on crée le fichier de configuration de WordPress
 wp config create --allow-root \
   --dbname=$SQL_DATABASE \
   --dbuser=$SQL_USER \
   --dbpass=$SQL_PASSWORD \
   --dbhost=mariadb:3306 \
+  --path='/var/www/wordpress'
+
+# Installation du site WordPress
+wp core install --allow-root \
+  --url="http://tlonghin.42.fr" \
+  --title="Mon Site WordPress" \
+  --admin_user=$WP_ADMIN_USER \
+  --admin_password=$WP_ADMIN_PASSWORD \
+  --admin_email=$WP_ADMIN_EMAIL \
   --path='/var/www/wordpress'
